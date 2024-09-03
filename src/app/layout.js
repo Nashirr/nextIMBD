6e606039e76d86358667bed772705cb2878ff00b
Nashir <johndoe@example.com>
2024-09-03 14:14:00 +0100

    Add Search Box

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
         <Header/>
         <Navbar/>
+        <SearchBox/>
         {children}
         </Providers>
         </body>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,11 +6,13 @@ import {FiThumbsUp} from 'react-icons/fi';
 export default function Card({ result }) {
   return (
     <div className='group cursor-pointer sm:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
-        <Link href={`/movie/${result.id}`}>
+        <Link as="image" href={`/movie/${result.id}` }>
         <Image
         src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
         width={500}
         height={300}
+        priority={true}
+        alt='poster'
         className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300' >
 
         </Image>
